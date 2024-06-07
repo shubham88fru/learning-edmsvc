eab612824c4ab48798a3ac55bda4cbbd90704a01
shubham88fru <shubham88fru@yahoo.com>
2024-06-07 13:50:46 -0500

    setup eureka

---
+++ DiscoveryServer/src/main/java/org/learning/edmsvc/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,15 @@
+package org.learning.edmsvc.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer //enable this project as a eureka server.
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryServerApplication.class, args);
+    }
+
+}
