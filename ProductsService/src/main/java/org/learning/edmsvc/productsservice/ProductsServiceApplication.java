f4fbe591324b5c054c8351d4e54a781d9123e757
shubham88fru <shubham88fru@yahoo.com>
2024-06-07 14:08:55 -0500

    eureka server and client setup and test

--- ProductsService/src/main/java/org/learning/edmsvc/productsservice/ProductsServiceApplication.java
+++ ProductsService/src/main/java/org/learning/edmsvc/productsservice/ProductsServiceApplication.java
@@ -2,7 +2,9 @@ package org.learning.edmsvc.productsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient //enable this project as a eureka client.
 @SpringBootApplication
 public class ProductsServiceApplication {
 
