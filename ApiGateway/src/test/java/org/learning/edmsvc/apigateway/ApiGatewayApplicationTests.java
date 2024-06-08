37c3c9eb3400e127b711a6c60436a7b5148a1169
shubham88fru <shubham88fru@yahoo.com>
2024-06-08 14:57:37 -0500

    initialize spring cloud api gateway

---
+++ ApiGateway/src/test/java/org/learning/edmsvc/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learning.edmsvc.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
