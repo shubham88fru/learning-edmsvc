46a12432997997e3247e3e04e2674cdeab0d2f2a
shubham88fru <shubham88fru@yahoo.com>
2024-06-30 14:05:46 -0500

    product entity and repository setup

---
+++ ProductsService/src/main/java/org/learning/edmsvc/productsservice/core/data/ProductRepository.java
@@ -0,0 +1,7 @@
+package org.learning.edmsvc.productsservice.core.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<ProductEntity, String> {
+    ProductEntity findByProductId(String productId);
+}
